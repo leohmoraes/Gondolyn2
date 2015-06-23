dd623bd06a5ff8b4a756fe4d91ef02ec0e966939
mlantz <mattlantz@gmail.com>
2015-06-23 12:13:57 -0400

    UI Improvements

--- resources/views/account/forgot-password.blade.php
+++ resources/views/account/forgot-password.blade.php
@@ -7,19 +7,20 @@
 </div>
 
 <div class="raw100 raw-margin-top-24">
-    <div class="raw25 raw-left raw-block-10"></div>
-    <div class="raw50 raw-left">
-        <p>Please enter your email for us to send you a brand new password.</p>
-        <form id="forgotPassword" method="post" action="{{ URL::to('forgot/password/request') }}">
-            <?php echo Form::token(); ?>
-            <div class="raw100 raw-left">
-                <label for="old_password">Email Address</label>
-                <input type="email" name="email" class="form-control" placeholder="Your Email Address">
-            </div>
-            <div class="raw100 raw-left raw-margin-top-16">
-                <input id="update" type="submit" class="btn btn-primary raw-right" value="Send New Password">
-            </div>
-        </form>
+    <div class="row">
+        <div class="col-md-6 col-md-offset-3">
+            <p>Please enter your email for us to send you a brand new password.</p>
+            <form id="forgotPassword" method="post" action="{{ URL::to('forgot/password/request') }}">
+                <?php echo Form::token(); ?>
+                <div class="raw100 raw-left">
+                    <label for="old_password">Email Address</label>
+                    <input type="email" name="email" class="form-control" placeholder="Your Email Address">
+                </div>
+                <div class="raw100 raw-left raw-margin-top-16">
+                    <input id="update" type="submit" class="btn btn-primary raw-right" value="Send New Password">
+                </div>
+            </form>
+        </div>
     </div>
 </div>
 
--- resources/views/account/login.blade.php
+++ resources/views/account/login.blade.php
@@ -2,29 +2,29 @@
 
 @section('content')
 
-<div class="raw25 raw-m-10 raw-left raw-block-100"></div>
-
-<div class="raw50 raw-m-80 raw-left">
-    <h2>Login</h2>
-    <br>
-    <form role="form" method="post" action="{{ URL::to('login/request') }}">
-        <?php echo Form::token(); ?>
-        <div class="form-group">
-            <label for="Email">Email</label>
-            <input type="email" name="email" class="form-control" id="Email" placeholder="Email Address">
+    <div class="row">
+        <div class="col-md-6 col-md-offset-3">
+            <h2>Login</h2>
+            <br>
+            <form role="form" method="post" action="{{ URL::to('login/request') }}">
+                <?php echo Form::token(); ?>
+                <div class="form-group">
+                    <label for="Email">Email</label>
+                    <input type="email" name="email" class="form-control" id="Email" placeholder="Email Address">
+                </div>
+                <div class="form-group">
+                    <label for="Password">Password</label>
+                    <input type="password" name="password" class="form-control" id="Password" placeholder="Password">
+                </div>
+                <div class="checkbox">
+                    <label for="RememberMe">
+                        <input type="checkbox" name="remember_me" id="RememberMe"> Remember Me
+                    </label>
+                </div>
+                <button type="submit" class="btn btn-primary raw-margin-top-24 raw-right">Login</button>
+            </form>
+            <button type="submit" class="btn btn-info raw-left raw-margin-top-24" onclick="window.location='{{ URL::to('forgot/password') }}'">Forgot Password</button>
         </div>
-        <div class="form-group">
-            <label for="Password">Password</label>
-            <input type="password" name="password" class="form-control" id="Password" placeholder="Password">
-        </div>
-        <div class="checkbox">
-            <label for="RememberMe">
-                <input type="checkbox" name="remember_me" id="RememberMe"> Remember Me
-            </label>
-        </div>
-        <button type="submit" class="btn btn-primary raw-right">Login</button>
-    </form>
-    <button type="submit" class="btn btn-info raw-left" onclick="window.location='{{ URL::to('forgot/password') }}'">Forgot Password</button>
-</div>
+    </div>
 
 @stop
