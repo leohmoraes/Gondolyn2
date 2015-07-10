df7349c9e18ac8ee806f815b857fe8f61397082d
mlantz <mattlantz@gmail.com>
2015-07-10 10:47:34 -0400

    Predictive Typeahead example

--- resources/views/admin/form.blade.php
+++ resources/views/admin/form.blade.php
@@ -8,6 +8,11 @@
 
     {!! Form::open(array('files'=> true, 'url' => 'admin/submit/form')) !!}
 
+    <div class="form-group">
+        <label>Automobile</label>
+        <input class="form-control typeahead" placeholder="Brand of Automobile">
+    </div>
+
     {!! FormMaker::fromTable('users', null, Config::get('forms.shipping')); !!}
 
     {!! Form::submit('Submit') !!}
@@ -15,5 +20,37 @@
     {!! Form::close() !!}
 
 </div>
+
+@stop
+
+@section('javascript')
+
+    {!! Minify::javascript(url('/js/typeahead.bundle.js')) !!}
+
+    @parent
+
+    <script type="text/javascript">
+
+    var cars = [
+        'Audi',
+        'Mercedes',
+        'Bently',
+        'Volvo',
+        'BMW',
+        'Ford',
+        'Honda',
+        'Toyota',
+        'GMC'
+    ];
+
+    $('.typeahead').typeahead({
+        minLength: 1,
+        highlight: true
+    },
+    {
+        name: 'cars',
+        source: typeaheadMatcher(cars)
+    });
+    </script>
 
 @stop
